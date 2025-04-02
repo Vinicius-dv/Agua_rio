e7cbfe40171faf6b5a6d9705def59ace03d6a6d7
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-04-02 17:23:48 -0300

    criando o formulario

--- meuscript.js
+++ meuscript.js
@@ -1,15 +1,32 @@
-const menuHamburguer = document.getElementById("menu_hamburguer")
-const mainMobile = document.getElementById("main_mobile")
-const menuItens = document.querySelector(".menu_itens")
 
-menuHamburguer.addEventListener("click", () => {
-    mainMobile.classList.toggle("show")
-    menuItens.classList.toggle("open")
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const btn_enviar = document.getElementById("btn_enviar")
 
-mainMobile.addEventListener('click',(e)=>{
-    if(e.target ===mainMobile){
-        mainMobile.classList.remove('show')
-        menuItens.classList.remove("open")
-    }
+    btn_enviar.addEventListener("click", function(e) {
+        e.preventDefault()
+
+        const form = document.getElementById("form")
+        if (form) {
+            form.addEventListener("submit", function(event) {
+                event.preventDefault()
+            })
+        }
+
+        const nome = document.getElementById("nome").value.trim()
+        const cpf = document.getElementById("cpf").value.trim()
+        const matricula = document.getElementById("matricula").value.trim()
+        const whatsapp = document.getElementById("whatsapp").value.trim()
+        const email = document.getElementById("email").value.trim()
+
+        if (!nome || !cpf || !matricula || !whatsapp || !email) {
+            alert("Preencha todos os campos antes de enviar.")
+            return
+        }
+
+        const numero = "5516991950657";
+        const mensagem = `Olá, segue meus dados:\n\n👤 Nome: ${nome}\n🆔 CPF: ${cpf}\n📌 Matrícula: ${matricula}\n📱 WhatsApp: ${whatsapp}\n📧 E-mail: ${email}`
+
+        const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`
+        window.open(url, "_blank")
+    })
 })
